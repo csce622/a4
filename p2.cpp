7881bfe5f5468684679209b92ed0743ed67dfca4
Damien Lebrun-Grandie <dalg24@gmail.com>
2010-11-18 14:45:24 -0600

    2. partially solved
    [+] use of push_back therefore works only for back extensible sequences...

--- p2.cpp
+++ p2.cpp
@@ -3,7 +3,7 @@
  * CSCE-622: Generic Programming -- Assignment 4
  *
  * @author Damien L-G <lebrungd@neo.tamu.edu>
- * @date November 12, 2010
+ * @date November 16, 2010
  */
 
 #include <iostream>
@@ -25,30 +25,22 @@
 #include <boost/mpl/if.hpp>
 #include <boost/type_traits/is_integral.hpp>
 #include <boost/type_traits/is_complex.hpp>
+#include <boost/mpl/iter_fold.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/begin.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/reverse_fold.hpp>
+#include <boost/mpl/list_c.hpp>
+#include <boost/mpl/deque.hpp>
+#include <boost/mpl/vector_c.hpp>
+#include <boost/mpl/push_front.hpp>
+#include <boost/mpl/push_back.hpp>
+#include <boost/mpl/next_prior.hpp>
+#include <boost/mpl/clear.hpp>
 
 using namespace boost;
-using mpl::placeholders::_;
 using mpl::placeholders::_1;
 using mpl::placeholders::_2;
-
-
-template <typename T1, typename T2> 
-struct greater { 
-  typedef typename mpl::if_c<  
-    !(sizeof(T1) < sizeof(T2)), 
-    mpl::true_, 
-    mpl::false_ 
-  >::type type; 
-}; 
-
-
-BOOST_MPL_ASSERT(( is_integral<char> ));
-BOOST_MPL_ASSERT(( is_integral<int> ));
-BOOST_MPL_ASSERT(( is_integral<long> ));
-BOOST_MPL_ASSERT(( mpl::not_< is_integral<float> > ));
-BOOST_MPL_ASSERT(( mpl::not_< is_integral<double> > ));
-BOOST_MPL_ASSERT(( mpl::not_< is_complex<double> > ));
-BOOST_MPL_ASSERT(( is_complex< std::complex<double> > ));
 
 template <
   typename T1, 
@@ -56,7 +48,7 @@ template <
   bool T1_is_integral = is_integral<T1>::value,
   bool T2_is_integral = is_integral<T2>::value
 >
-struct least_upper_bound {
+struct upper {
   typedef typename mpl::if_c<
     (sizeof(T1)>sizeof(T2)),
     T1,
@@ -64,71 +56,219 @@ struct least_upper_bound {
   >::type type;
 };
 
-template<typename F1, typename F2>
+template <typename F1, typename F2>
 struct xor_ : mpl::and_<
   mpl::or_<F1, F2>,
   mpl::not_< mpl::and_<F1, F2> >
 > {};
 
-// not determined yet
+BOOST_MPL_ASSERT_NOT(( xor_< mpl::true_, mpl::true_ > ));
+BOOST_MPL_ASSERT(( xor_< mpl::true_, mpl::false_ > ));
+BOOST_MPL_ASSERT(( xor_< mpl::false_, mpl::true_ > ));
+BOOST_MPL_ASSERT_NOT(( xor_< mpl::false_, mpl::false_ > ));
+
+// not very elegant but works fine :D
 template <typename T1, typename T2>
-struct least_upper_bound<T1, T2, false, false> {
-  typedef void type;
+struct tweak {
+  typedef typename mpl::if_<
+    is_complex<T1>,
+    T1,
+    T2
+  >::type type;
+};
+
+template <>
+struct tweak< std::complex<float>, double > {
+  typedef std::complex<double> type;
+};
+
+template <>
+struct tweak< double, std::complex<float> > {
+  typedef std::complex<double> type;
 };
 
 template <typename T1, typename T2>
-struct least_upper_bound<T1, T2, true, false> {
+struct upper<T1, T2, false, false> {
+  typedef typename mpl::if_<
+    xor_<
+      is_complex<T1>,
+      is_complex<T2>
+    >,
+    typename tweak<
+      T1, 
+      T2
+    >::type,
+    typename mpl::if_c<
+      (sizeof(T1)>sizeof(T2)),
+      T1,
+      T2
+    >::type
+  >::type type;
+};
+
+template <typename T1, typename T2>
+struct upper<T1, T2, true, false> {
   typedef T2 type;
 };
 
 template <typename T1, typename T2>
-struct least_upper_bound<T1, T2, false, true> {
+struct upper<T1, T2, false, true> {
   typedef T1 type;
 };
 
-//BOOST_MPL_ASSERT(( is_same< least_upper_bound<double, double>::type, double > ));
-//BOOST_MPL_ASSERT(( is_same< least_upper_bound<double, float>::type, double > ));
-//BOOST_MPL_ASSERT(( is_same< least_upper_bound<float, double>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<double, long>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<long, double>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<double, int>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<int, double>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<double, char>::type, double > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<char, double>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, std::complex<float> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, double >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< double, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, float >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< float, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, long >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< long, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, int >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< int, std::complex<double> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<double>, char >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< char, std::complex<double> >::type, std::complex<double> > ));
 
-//BOOST_MPL_ASSERT(( is_same< least_upper_bound<float, float>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<float, long>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<long, float>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<int, float>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<float, int>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<char, float>::type, float > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<float, char>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, std::complex<float> >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, double >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< double, std::complex<float> >::type, std::complex<double> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, float >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< float, std::complex<float> >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, long >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< long, std::complex<float> >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, int >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< int, std::complex<float> >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< std::complex<float>, char >::type, std::complex<float> > ));
+BOOST_MPL_ASSERT(( is_same< upper< char, std::complex<float> >::type, std::complex<float> > ));
 
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<long, long>::type, long > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<long, int>::type, long > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<int, long>::type, long > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<long, char>::type, long > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<char, long>::type, long > ));
+BOOST_MPL_ASSERT(( is_same< upper<double, double>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<double, float>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<float, double>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<double, long>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<long, double>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<double, int>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<int, double>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<double, char>::type, double > ));
+BOOST_MPL_ASSERT(( is_same< upper<char, double>::type, double > ));
 
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<int, int>::type, int > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<int, char>::type, int > ));
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<char, int>::type, int > ));
+BOOST_MPL_ASSERT(( is_same< upper<float, float>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<float, long>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<long, float>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<int, float>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<float, int>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<char, float>::type, float > ));
+BOOST_MPL_ASSERT(( is_same< upper<float, char>::type, float > ));
 
-BOOST_MPL_ASSERT(( is_same< least_upper_bound<char, char>::type, char > ));
+BOOST_MPL_ASSERT(( is_same< upper<long, long>::type, long > ));
+BOOST_MPL_ASSERT(( is_same< upper<long, int>::type, long > ));
+BOOST_MPL_ASSERT(( is_same< upper<int, long>::type, long > ));
+BOOST_MPL_ASSERT(( is_same< upper<long, char>::type, long > ));
+BOOST_MPL_ASSERT(( is_same< upper<char, long>::type, long > ));
 
-// DUMMY VERSION WORKS FINE //////////////
-template <typename T1, typename T2>
-struct promote_dummy {
-  typedef T2 type;
+BOOST_MPL_ASSERT(( is_same< upper<int, int>::type, int > ));
+BOOST_MPL_ASSERT(( is_same< upper<int, char>::type, int > ));
+BOOST_MPL_ASSERT(( is_same< upper<char, int>::type, int > ));
+
+BOOST_MPL_ASSERT(( is_same< upper<char, char>::type, char > ));
+
+
+
+/// a la main //////////////////////////////
+template <typename S1, typename S2>
+struct foo {
+  BOOST_MPL_ASSERT_MSG( (is_same< typename mpl::size<S1>::type, typename mpl::size<S2>::type >::value), 
+    SEQUENCES_MUST_HAVE_SAME_SIZE, (S1, S2) );
+  typedef typename mpl::begin<S1>::type iter_S1;
+  typedef typename mpl::begin<S2>::type iter_S2;
+  typedef typename upper<
+    typename mpl::deref<iter_S1>::type,
+    typename mpl::deref<iter_S2>::type
+  >::type type1;
+  typedef typename upper<
+    typename mpl::deref< typename mpl::next<iter_S1>::type >::type,
+    typename mpl::deref< typename mpl::next<iter_S2>::type >::type
+  >::type type2;
 };
-/////////////////////////////////////////
 
-typedef mpl::lambda<_2>::type myfunc;
+BOOST_MPL_ASSERT(( is_same< foo< mpl::vector<double, int>, mpl::vector<long, float> >::type1, double > ));
+BOOST_MPL_ASSERT(( is_same< foo< mpl::vector<double, int>, mpl::vector<long, float> >::type2, float > ));
 
-template <typename T1, typename T2>
-struct promote : mpl::apply<myfunc, T1, T2> {};
+/// boucle recopie une sequence ///////////////////
+template <typename Sequence, typename Iterator, typename Copy, 
+  bool Condition = is_same<typename mpl::end<Sequence>::type, Iterator>::value >
+struct bar {
+  typedef typename bar<
+    Sequence, 
+    typename mpl::next<Iterator>::type, 
+    typename mpl::push_back<
+      Copy, 
+      typename mpl::deref<Iterator>::type
+    >::type
+  >::type type;
+};
 
-BOOST_MPL_ASSERT(( is_same<promote<double, char>::type, char > ));
+template <typename Sequence, typename Iterator, typename Copy>
+struct bar<Sequence, Iterator, Copy, true> {
+  typedef Copy type;
+};
+
+typedef mpl::vector<double, char, int> enfer;
+typedef bar< enfer::type, mpl::begin<enfer::type>::type, mpl::vector<> >::type damnation;
+//BOOST_MPL_ASSERT(( is_same< damnation, enfer > ));
+BOOST_MPL_ASSERT(( mpl::equal< damnation, enfer > ));
+
+
+/// eureka ///////////////////////////////
+template <
+  typename Sequence1, 
+  typename Sequence2, 
+  typename Iterator1, 
+  typename Iterator2, 
+  typename Output, 
+  bool Condition = is_same<
+    typename mpl::end<Sequence1>::type, 
+    Iterator1
+  >::value
+>
+struct eureka {
+  typedef typename eureka<
+    Sequence1, 
+    Sequence2, 
+    typename mpl::next<Iterator1>::type, 
+    typename mpl::next<Iterator2>::type, 
+    typename mpl::push_back<
+      Output, 
+      typename upper<
+        typename mpl::deref<Iterator1>::type,
+        typename mpl::deref<Iterator2>::type
+      >::type
+    >::type
+  >::type type;
+};
+
+template <
+  typename Sequence1, 
+  typename Sequence2, 
+  typename Iterator1, 
+  typename Iterator2, 
+  typename Output>
+struct eureka<Sequence1, Sequence2, Iterator1, Iterator2, Output, true> {
+  typedef Output type;
+};
+
+/// encapsulate eureka -> promote ///////////////////////////////
+template <typename S1, typename S2>
+struct promote {
+  BOOST_MPL_ASSERT_MSG( (is_same< typename mpl::size<S1>::type, typename mpl::size<S2>::type >::value), SEQUENCES_MUST_HAVE_SAME_SIZE, () );
+  typedef typename eureka<
+    S1, 
+    S2, 
+    typename mpl::begin<S1>::type, 
+    typename mpl::begin<S2>::type, 
+    typename mpl::clear<S1>::type
+  >::type type;
+};
 
 
 
@@ -140,19 +280,23 @@ int main(int,char*[])
   std::cout << " # p2 # \n";
   std::cout << "######  \n";
 
-  // DUMMY VERSION WORKS FINE //////////////
+  // checking example in the assignment
   typedef mpl::vector<char, double, int> s1;
   typedef mpl::vector<double, double, long> s2;
-  BOOST_MPL_ASSERT_MSG( (mpl::equal<promote_dummy<s1,s2>::type, s2>::type::value), CASSE_COUILLE, () );
+  BOOST_MPL_ASSERT(( mpl::equal< promote<s1, s2>::type, s2 > ));
+
+  // another check
+  BOOST_MPL_ASSERT(( mpl::equal<
+    promote<
+      mpl::deque<double, int, char, long, long, float>,
+      mpl::deque<char, std::complex<float>, long, int, float, double>
+    >::type, 
+    mpl::deque<double, std::complex<float>, long, long, float, double> 
+  > ));
+
   // The following line will fail at run time.
   //assert(( mpl::equal<s1, s2>::type::value ));
-  /////////////////////////////////////////
   
-  BOOST_MPL_ASSERT(( mpl::less<mpl::int_<2>, mpl::int_<10> > ));
-
-  BOOST_MPL_ASSERT(( mpl::equal<myfunc::apply<s1, s2>, s1>::type::value ));
-
-  //BOOST_MPL_ASSERT(( mpl::equal<promote<s1, s2>, s2>::type ));
 
   std::cout << "char   " << sizeof(char  ) << "\n";
   std::cout << "int    " << sizeof(int   ) << "\n";
